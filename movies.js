2f232d12edf7952b104ff7f09b092bdbd20daa1a
Damian <Damiankohut@gmail.com>
2022-03-02 11:30:15 -0500

    added the search bar to the main poage

--- movies.js
+++ movies.js
@@ -3,4 +3,29 @@ window.addEventListener('DOMContentLoaded', () => {
     fetch(moviesAPI)
     .then(respone => respone.json())
     .then(data => console.log(data))
+    
+    const work = document.getElementById("submit2");
+    work.addEventListener('click', movieFinder )
 })
+function movieFinder() {
+    const input2 = document.getElementById('code2');
+    getpokemon(input2.value)
+}
+
+
+
+
+function getpokemon(pokemon){
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=3c8d31b949ad58738c6e56fd0522a70a&language=en-US&query=${pokemon}&page=1&include_adult=false`)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        document.getElementById('box').innerHTML = `
+        <h5>${data.results[0].original_title}</h5>
+        <img src = 'https://image.tmdb.org/t/p/w500/${data.results[0].poster_path}' width = "240px" height = "200px">
+        <p id="text">${data.results[0].overview}</p>
+        <p id = "text"> Release ${data.results[0].release_date} </p>
+        `
+    })
+}
