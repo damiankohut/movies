e1479c4ca7cae5ad2e461830e41b7d0f903a7947
Damian <Damiankohut@gmail.com>
2022-03-03 16:59:37 -0500

    fix styling and updated text

--- movies.js
+++ movies.js
@@ -46,7 +46,7 @@ function renderImages(data){
                 })
                 .then(function(data){
                 document.getElementById('link').innerHTML = `
-                <a href=${data.results.US.link}>Check where you can watch this</a>
+                <a href=${data.results.US.link} target="_blank" rel="noopener noreferrer">Check where you can watch this</a>
                 `
                 })
             }
