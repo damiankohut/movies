05b8ab0ac2bf1690573c0081cb93e8e251dd55ca
damiankohut <damiankohut@gmail.com>
2023-01-24 23:58:08 -0500

    test

--- movies.js
+++ movies.js
@@ -2,12 +2,12 @@ window.addEventListener('DOMContentLoaded', () => {
     const moviesAPI = "https://api.themoviedb.org/3/discover/movie?api_key=3c8d31b949ad58738c6e56fd0522a70a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
     fetch(moviesAPI)
     .then(respone => respone.json())
-    .then(data => renderImages(data.results))
+    .then(datsa => renderImages(data.results))
     
     const work = document.getElementById("submit2");
     work.addEventListener('click', movieFinder )
     
-
+//test
 
 
    })
